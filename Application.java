c641c710a6f5fdf5751d008f160b4a837c1e62bb
Ivan Korotkiy <vana0pub2@gmail.com>
2025-02-11 13:44:43 +0300

    добавлен пример

--- Application.java
+++ Application.java
@@ -1,8 +1,8 @@
 public class Application {
-    public void main(String arg[]) {
+    public static void main(String[] arg) {
         ConcreteStation sConcreteStation = new ConcreteStation();
         sConcreteStation.registerObs(new WeatherData());
         sConcreteStation.Update(36, 70, 776);
-        sConcreteStation.notify();
+        sConcreteStation.notifyObs();
     }
 }
